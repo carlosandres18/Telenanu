0056268ac2ff3166dda62ee331381e0ff24b19f8
carlosandres18 <48213475+carlosandres18@users.noreply.github.com>
2020-11-05 23:06:07 -0500

    Update site

---
+++ SitioTelenanu/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SitioWebChat.Models;
+
+
+namespace SitioWebChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+         
+            return View();
+        }
+
+        public IActionResult Chatbot()
+        {
+         
+            return View();
+        }
+
+        public IActionResult Preguntas()
+        {
+         
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+         
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+         
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+}
---
+++ SitioTelenanu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SitioWebChat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
